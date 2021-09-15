5ef071803c0bc9124525331cb1c5c19b26d5088e
World-Teck-Studio <47188391+World-Teck-Studio@users.noreply.github.com>
2021-09-16 01:13:23 +0200

    Update Final.sh

--- Final.sh
+++ Final.sh
@@ -23,10 +23,13 @@ whiptail --title "ETAPE 4" --msgbox "Téléchargement + installation de python \
 whiptail --title "ETAPE 5" --msgbox "Installation de google assistance\n---------------------------------\n\n Temps: 10 et 25 minutes \n\n Mode: Full Automatique" 12 78
 python -m pip install google_assistant_library-1.1.0-py2.py3-none-linux_armv7l.whl
 python -m pip install --upgrade google-auth-oauthlib[tool]
+clear
 whiptail --title "ETAPE 5" --msgbox "Installation de google assistance\n---------------------------------\n\n Information: TERMINER \n\n Vous pouvez passé a la derniere étapes" 12 78
 whiptail --title "ETAPE 6 FINAL" --msgbox "Activation du service + code d'access\n---------------------------------\n\n Temps: 5 et 10 minutes \n\n Mode: Full Manuel" 12 78
 google-oauthlib-tool --client-secrets code_secret_client.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
 clear
 whiptail --title "ETAPE 6" --msgbox "Activation du service + code d'access\n---------------------------------\n\n Information: TERMINER \n\n" 12 78
+clear
 whiptail --title "Félicitation" --msgbox "L'installation de google assitance est TERMINER\n---------------------------------\n\n Félicitation, le software est correctement installé \n\n il reste plusqu'a faire OK est dire OK GOOGLE." 12 78
+google-assistant-demo --device-model-id my-device-model
 fi
